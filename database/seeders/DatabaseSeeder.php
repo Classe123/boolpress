6ea45f207825dff1afa355311748f0f7d8d6d535
klelia <clelia.fradella@gmail.com>
2024-06-19 11:27:05 +0200

    call seeder example

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -17,6 +18,11 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
+        // ]);
+        // $this->call([
+        //     MovieSeeder::class,
+        //     ReviewSeeder::class,
+
         // ]);
     }
 }
